d19c190bc211837dc06bc6c8c6bd12e8a8db066c
Arnanil Patra <arnanilpat2002@gmail.com>
2026-02-12 12:58:54 +0530

    Add day-6 files

---
+++ day-6/server.js
@@ -0,0 +1,15 @@
+const app = require('./src/app');
+const mongoose = require("mongoose")
+
+function connectToDb() {
+    mongoose.connect("URI  /day-6")
+        .then(() => {
+            console.log("Connected to Database")
+        })
+}
+
+connectToDb()
+
+app.listen(3000, () => {
+    console.log("server is running on port 3000")
+})
